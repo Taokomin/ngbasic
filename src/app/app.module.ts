d2928ec6b7a6213085f354beb140e8c5265ac0e8
Taokomin <s6206021621015@email.kmutnb.ac.th>
2023-12-07 21:50:15 +0700

    pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaxMinMeterComponent } from './max-min-meter/max-min-meter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaxMinMeterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/max-min-meter/max-min-meter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-max-min-meter',
+  templateUrl: './max-min-meter.component.html',
+  styleUrl: './max-min-meter.component.css'
+})
+export class MaxMinMeterComponent {
+
+}
